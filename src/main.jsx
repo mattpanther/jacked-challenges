2d2bb0b900e41864fe7be54a45905a73c559db80
Matt Jansen <matt@rolekit.com>
2026-02-19 15:52:39 -0500

    Fix: add BrowserRouter basename for GitHub Pages routing

--- src/main.jsx
+++ src/main.jsx
@@ -38,7 +38,7 @@ class ErrorBoundary extends React.Component {
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ErrorBoundary>
-      <BrowserRouter>
+      <BrowserRouter basename="/jacked-challenges">
         <AuthProvider>
           <App />
         </AuthProvider>
